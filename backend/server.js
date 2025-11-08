52bc59f07badbd8021c392fe339549840823cacd
nikhilasaipreeti <nikhilasaipreeti2005@gmail.com>
2025-11-08 20:00:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,8 @@ const app = express();
 app.use(
     cors({
         origin: [
-            "https://smart-medicare-f.onrender.com", // your deployed frontend
-            "http://localhost:5173", 
-            "http://127.0.0.1:5173"
+            "https://smart-medicare-1.onrender.com", // your deployed frontend
+            "http://localhost:5173"
         ],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
